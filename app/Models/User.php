6499293ffe9ec02079ff2ed1bc2ced6e70d28573
ryan-20 <sarip.ryan17@gmail.com>
2023-04-02 15:16:17 +0000

    First Push to Repository

---
+++ app/Models/User.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class User extends Model{
+protected $table = 'customer';
+// column sa table
+protected $fillable = [
+'customer_name', 'customer_age', 'customer_sex'
+];
+}
